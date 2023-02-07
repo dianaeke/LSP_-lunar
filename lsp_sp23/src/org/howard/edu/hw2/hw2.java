46cc0c0990495698158ca162f4688f865b688931
dianaeke <104582209+dianaeke@users.noreply.github.com>
2023-02-06 22:25:06 -0500

    Update and rename lsp_sp23/src/org/howard/edu/hw2.java to lsp_sp23/src/org/howard/edu/hw2/hw2.java
    code

---
+++ lsp_sp23/src/org/howard/edu/hw2/hw2.java
@@ -0,0 +1,86 @@
+/** Uchenna Ekechukwu
+**/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* wordcount class */
+class WordCount
+{
+  Scanner objt = null;
+  Scanner file = null;
+  
+  ArrayList<String> words = null;
+  ArrayList<Integer> w_count = null;
+  
+  public WordCount(String file_name) throws FileNotFoundException
+  {
+    objt = new Scanner(System.in);
+    file = new Scanner(new File(filename));
+    words = new ArrayList<String>();
+    w_count = new ArrayList<Integer>();
+  }
+  
+  void counter()
+  {
+    String str;
+    int c = 0;
+    String s = "";
+    
+    while(file.hasNext())
+    {
+      s = "";
+      str = file.next();
+      if (str.length() > 3)
+      {
+        for (int i = 0; i < str.length(); i++)
+        {
+          if (Character.isAlphabetic(str.charAt(i)))
+          {
+            s += str.charAt(i);
+          }
+        }
+        if (!words.contains(s))
+        {
+          words.add(s);
+          w_count.add(1);
+        }
+        else
+        {
+          int indx = words.indexOf(str);
+          w_count.set(indx, w_count.get(indx) + 1);
+        }
+      }
+    }
+    file.close();
+  }
+  
+  /* print out words and count */
+  void print_count()
+  {
+    System.out.println("Word Count: ");
+    for(int i = 0; i < words.size(); i++)
+    {
+      System.out.printf("%6d %-10s/n", w_count.get(i), words.get(i))
+      }
+  }
+  
+  public void words_counter()
+  {
+    counter();
+    print_count();
+  }
+}
+public class Main
+{
+  public static void main(String[] args) throws FileNotFoundException
+  {
+    WordCount wc = new WordCount("words.txt");
+    wc.words_counter();
+  }
+}
+  
+              
+  
